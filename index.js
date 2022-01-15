3ea7270cdf08c5f7644a45bcb9e074add811ac15
solstice23 <solstice233@gmail.com>
2022-01-15 19:26:24 +0800

    Cache support

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 import express from 'express';
 import path from 'path';
+import NodeCache from 'node-cache';
 import * as libs from './libs.js';
 import * as render from './render.js';
 import * as api from './api.js';
 
 const __dirname = path.resolve();
 
+var cacheControl = new NodeCache({ stdTTL: 600, checkperiod: 600, deleteOnExpire: true });
+
 var app = express();
 
 app.use('/', express.static(path.join(__dirname, '/static')));
 
-app.get('/card', async function(req, res) {
+app.get('/card', async function (req, res) {
     res.set({
         'Content-Type': 'image/svg+xml',
         'Cache-Control': 'public, max-age=3600'
     });
     let username = req.query.user ?? "";
     let playmode = req.query.mode ?? "std";
+
+    let cacheKey = `{username}|{playmode}`;
+    if (req.headers['cache-control'] != 'no-cache' && cacheControl.has(cacheKey)) {
+        res.send(cacheControl.get(cacheKey));
+        return;
+    }
+
     let userData = await api.getUser(username, playmode);
     if (userData.error) {
         res.send(render.getErrorSVG("Error: " + userData.error));
@@ -40,7 +50,7 @@ app.get('/card', async function(req, res) {
         userCoverImageBase64 = await libs.getResizdCoverBase64(userCoverImage, 550, 120, blur);
         [width, height] = [550, 320];
     }
-    
+
     userData.options = {
         language: req.query.lang ?? "cn",
         animation: (req.query.animation != undefined && req.query.animation != 'false'),
@@ -52,11 +62,14 @@ app.get('/card', async function(req, res) {
         color_hue: parseInt(req.query.hue ?? 333),
     }
 
+    let svg = "";
     if (isMini) {
-        res.send(render.getRenderedSVGMini(userData, avatarBase64, userCoverImageBase64));
-    }else{
-        res.send(render.getRenderedSVGFull(userData, avatarBase64, userCoverImageBase64));
+        svg = render.getRenderedSVGMini(userData, avatarBase64, userCoverImageBase64);
+    } else {
+        svg = render.getRenderedSVGFull(userData, avatarBase64, userCoverImageBase64);
     }
+    cacheControl.set(cacheKey, svg);
+    res.send(svg);
 });
 
 app.listen(process.env.PORT || 3000);
